033dd0357afd3aa1ee4e82e21efa07fa1f4d3067
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-07-11 17:11:00 +0600

    added to Special Products section in home pages

--- src/components/ui/specialProducts.tsx
+++ src/components/ui/specialProducts.tsx
@@ -1,68 +1,92 @@
-import Carousel from "react-multi-carousel";
+import { Card, CardContent } from "./card";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./carousel";
+
 
 const specialProducts = () => {
-    // const arr = [
-    //     {
-    //         img: "https://i.ibb.co/dmdZ0h6/img1.webp"
-
-    //     },
-    // ]
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    }
-
 
     const products = [
         {
-            image:"https://i.ibb.co/dmdZ0h6/img1.webp",
-            title: 'Product 1',
-            price: '$100'
+            id: 1,
+            name: "Sirac Plus 65L Backpack",
+            image: "https://i.ibb.co/WWfWLCn/slider2-9f305fca-5d30-4456-b297-cccd1fdd59c8-2000x.webp",
+            price: "$100",
+            description:"Whether you’re heading out on your first backpacking adventure or need a high comfort pack that will keep you organised out on the mountain, the Sirac Plus 65 is sturdy, stable and offers convenient storage features. The adjustable Air Contour™ X back system is designed to optimise your comfort, using strategically positioned foam panels which cushion your back and form air channels to keep you cool and dry."
         },
         {
-            image: 'product2.jpg',
-            title: 'Product 2',
-            price: '$150'
+            id: 1,
+            name: "Sirac Plus 65L Backpack",
+            image: "https://i.ibb.co/WWfWLCn/slider2-9f305fca-5d30-4456-b297-cccd1fdd59c8-2000x.webp",
+            price: "$100",
+            description:"Whether you’re heading out on your first backpacking adventure or need a high comfort pack that will keep you organised out on the mountain, the Sirac Plus 65 is sturdy, stable and offers convenient storage features. The adjustable Air Contour™ X back system is designed to optimise your comfort, using strategically positioned foam panels which cushion your back and form air channels to keep you cool and dry."
         },
         {
-            image: 'product3.jpg',
-            title: 'Product 3',
-            price: '$200'
+            id: 1,
+            name: "Sirac Plus 65L Backpack",
+            image: "https://i.ibb.co/WWfWLCn/slider2-9f305fca-5d30-4456-b297-cccd1fdd59c8-2000x.webp",
+            price: "$100",
+            description:"Whether you’re heading out on your first backpacking adventure or need a high comfort pack that will keep you organised out on the mountain, the Sirac Plus 65 is sturdy, stable and offers convenient storage features. The adjustable Air Contour™ X back system is designed to optimise your comfort, using strategically positioned foam panels which cushion your back and form air channels to keep you cool and dry."
         },
         {
-            image: 'product4.jpg',
-            title: 'Product 4',
-            price: '$250'
+            id: 1,
+            name: "Sirac Plus 65L Backpack",
+            image: "https://i.ibb.co/WWfWLCn/slider2-9f305fca-5d30-4456-b297-cccd1fdd59c8-2000x.webp",
+            price: "$100",
+            description:"Whether you’re heading out on your first backpacking adventure or need a high comfort pack that will keep you organised out on the mountain, the Sirac Plus 65 is sturdy, stable and offers convenient storage features. The adjustable Air Contour™ X back system is designed to optimise your comfort, using strategically positioned foam panels which cushion your back and form air channels to keep you cool and dry."
         },
+        {
+            id: 1,
+            name: "Sirac Plus 65L Backpack",
+            image: "https://i.ibb.co/WWfWLCn/slider2-9f305fca-5d30-4456-b297-cccd1fdd59c8-2000x.webp",
+            price: "$100",
+            description:"Whether you’re heading out on your first backpacking adventure or need a high comfort pack that will keep you organised out on the mountain, the Sirac Plus 65 is sturdy, stable and offers convenient storage features. The adjustable Air Contour™ X back system is designed to optimise your comfort, using strategically positioned foam panels which cushion your back and form air channels to keep you cool and dry."
+        },
+        {
+            id: 1,
+            name: "Sirac Plus 65L Backpack",
+            image: "https://i.ibb.co/WWfWLCn/slider2-9f305fca-5d30-4456-b297-cccd1fdd59c8-2000x.webp",
+            price: "$100",
+            description:"Whether you’re heading out on your first backpacking adventure or need a high comfort pack that will keep you organised out on the mountain, the Sirac Plus 65 is sturdy, stable and offers convenient storage features. The adjustable Air Contour™ X back system is designed to optimise your comfort, using strategically positioned foam panels which cushion your back and form air channels to keep you cool and dry."
+        },
+        {
+            id: 1,
+            name: "Sirac Plus 65L Backpack",
+            image: "https://i.ibb.co/WWfWLCn/slider2-9f305fca-5d30-4456-b297-cccd1fdd59c8-2000x.webp",
+            price: "$100",
+            description:"Whether you’re heading out on your first backpacking adventure or need a high comfort pack that will keep you organised out on the mountain, the Sirac Plus 65 is sturdy, stable and offers convenient storage features. The adjustable Air Contour™ X back system is designed to optimise your comfort, using strategically positioned foam panels which cushion your back and form air channels to keep you cool and dry."
+        },
+       
+        
+      
         // Add more products as needed
-    ]
+    ];
     return (
         <div className=" py-14">
-            <h2 className=" text-center font-semibold text-3xl">Special Products</h2>
-            <div className=" p-20">
-                <Carousel responsive={responsive} infinite autoPlay>
-                    {
-                        products.map(items => <div className="product-card">
-                            <img src={items.image} alt={items.title} className="product-image" />
-                            <h3 className="product-title">{items.title}</h3>
-                            <p className="product-price">{items.price}</p>
-                        </div>)
-                    }
+            <h2 className=" text-center font-semibold text-3xl">Special <span className=" text-orange-500">Products</span></h2>
+            <div className="py-10 flex justify-center">
+                <Carousel
+                    opts={{
+                        align: "start",
+                    }}
+                    className="w-full max-w-6xl"
+                >
+                    <CarouselContent>
+                        {products.map((product) => (
+                            <CarouselItem key={product.id} className="md:basis-1/2 lg:basis-1/4 ">
+                                <div className="">
+                                    <Card className=" hover:shadow-xl">
+                                        <CardContent className="flex flex-col p-3 Productsp-1 space-y-3 ">
+                                            <img src={product.image} alt={product.name} className="w-full h-48 object-cover mb-4 rounded-md " />
+                                            <h3 className="text-lg font-semibold">{product.name.length > 10 ? product.name.slice(0 ,20 )+" ..." : product.name}</h3>
+                                            <p className="text-gray-700">{product?.description?.length > 100 ? product.description?.slice(0,100)+"...." : ""}</p>
+                                            <p className="text-orange-500 text-xl font-semibold">{product.price}</p>
+                                        </CardContent>
+                                    </Card>
+                                </div>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious className="absolute -left-7 p-2 text-white bg-black bg-opacity-50 rounded-full hover:bg-opacity-75" />
+                    <CarouselNext className="absolute -right-7 p-2 text-white bg-black bg-opacity-50 rounded-full hover:bg-opacity-75" />
                 </Carousel>
             </div>
         </div>
